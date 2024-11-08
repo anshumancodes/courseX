945ff93b3f490331240130021aaddc9ff4eab569
anshumancodes <anshumanprof01@gmail.com>
2024-11-09 02:46:11 +0530

    add new routers in app.js

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ app.route("/").get((req, res) => {
 });
 
 import userRoutes from "./routes/user.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import courseRoutes from "./routes/course.routes.js";
 app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/course", courseRoutes);
 
 export default app;
