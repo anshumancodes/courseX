a40a7ce15bf4da59de60c3a6bc02184541f1f38d
anshumancodes <anshumanprof01@gmail.com>
2024-11-09 02:44:18 +0530

    add new routes in admin routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { AdminLogin,AdminSignup } from "../controllers/Admin.controller.js";
+import {verifyAdminAuth} from "../middlewares/auth.middleware.js";
+import { CreateCourse,DeleteCourse } from "../controllers/Course.controller.js";
 
 const router = Router();
 
 router.route("/login").post(AdminLogin);
 router.route("/signup").post(AdminSignup);
+router.route("/admin/create-Course").post(verifyAdminAuth,CreateCourse);
+router.route("/admin/delete-Course").post(verifyAdminAuth,DeleteCourse);
+
 
 export default router;
